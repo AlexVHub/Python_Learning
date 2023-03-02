b0a7387fce137543cc35aabb46750fb60855d345
AlexV <alexvdata@gmail.com>
2023-03-02 23:29:04 +1100

    Done

--- Побочная диагональ.py
+++ Побочная диагональ.py
@@ -1,2 +1,10 @@
-n = 4
-matrix = []
+n = int(input())
+matrix = [[0] * n for _ in range(n)]
+
+for i in range(n):
+    matrix[i][-i - 1] = 1
+    for j in range(i):
+        matrix[i][-j - 1] = 2
+
+for row in matrix:
+    print(*matrix)
