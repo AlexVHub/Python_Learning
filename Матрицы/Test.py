3fa7529a435dba8735a8d203cd9caf665968e43b
AlexV <alexvdata@gmail.com>
2023-03-09 01:16:42 +1100

    Done

--- Матрицы/Test.py
+++ Матрицы/Test.py
@@ -1,27 +1,21 @@
-n, m = [int(i) for i in input().split()]
+n, m = 3, 4
 matrix = [[0] * m for _ in range(n)]
 count = 1
 row = 0
 col = 0
+k = 0
 
-for i in range(20):
-    print(row, col, i)
-    if col < 0:
-        col = row
-    if i == 1:
-        row = 0
-    print(row, col, i)
-    if col > (m - 1):
-        col -= 2
-        row += 1
-    print(row, col, i)
-    matrix[row][col] = count
-    print(matrix)
-    count += 1
-    col -= 1
-    row += 1
-    if row > n - 1:
-        row = 0
+for i in range(5):
+    while row < n and col >= 0:
+        matrix[row][col] = count
+        count += 1
+        if col <= 0:
+            row, col = col, row
+            col += 1
+            break
+        else:
+            row += 1
+            col -= 1
 
 
 for i in range(n):
