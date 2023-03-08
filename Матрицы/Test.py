733fa9abb9a5a4bf77231d1828098e54b9810f4e
AlexV <alexvdata@gmail.com>
2023-03-09 04:16:26 +1100

    Done

--- Матрицы/Test.py
+++ Матрицы/Test.py
@@ -1,22 +1,27 @@
-n, m = 3, 4
+n, m = 3, 6
 matrix = [[0] * m for _ in range(n)]
 count = 1
 row = 0
 col = 0
-k = 0
+temp = 0
+flag = False
 
-for i in range(5):
-    while row < n and col >= 0:
-        matrix[row][col] = count
-        count += 1
-        if col <= 0:
-            row, col = col, row
-            col += 1
-            break
-        else:
-            row += 1
-            col -= 1
-
+while count < 13:
+    matrix[row][col] = count
+    count += 1
+    if flag == False:
+        col += 1
+        flag = True
+        continue
+    col -= 1
+    row += 1
+    if col < 0 or row > n - 1:
+        temp = col
+#        if row == n:
+#            row -= 1
+#            temp += 1
+        col = row
+        row = temp + 1
 
 for i in range(n):
     for j in range(m):
