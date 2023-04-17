33260d7fdd3b5b3f01ab8443f871f6125cc69f9d
AlexVHub <alexvdata@gmail.com>
2023-04-18 03:43:20 +1100

    Done

--- Словари/Права доступа.py
+++ Словари/Права доступа.py
@@ -1,10 +1,3 @@
 acl = {'write': 'W', 'read': 'R', 'execute': 'X'}
-files = {}
-
-for i in range(int(input())):
-    s = input().split()
-    files[s[0]] = s[1:]
-
-for j in range(int(input())):
-    r = input().split()
-    print('OK' if acl[r[0]] in files[r[1]] else 'Access denied')
+files = {k: v for _ in range(int(input())) for k, *v in [input().split()]}
+[print('OK' if acl[a] in files[f] else 'Access denied') for _ in range(int(input())) for a, f in [input().split()]]
