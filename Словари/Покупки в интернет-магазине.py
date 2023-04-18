c49dd05048db068e4d702dbf76829c5d0de3f938
AlexVHub <alexvdata@gmail.com>
2023-04-19 04:47:56 +1100

    Done

--- Словари/Покупки в интернет-магазине.py
+++ Словари/Покупки в интернет-магазине.py
@@ -1,19 +1,7 @@
-d1 = {}
+buyers = {}
+a = {buyers.setdefault(buyer + ':', dict()).setdefault(product, []).append(int(count)) for i in range(int(input())) for
+     buyer, product, count in [input().split()]}
 
-for i in range(int(input())):
-    l = input().split()
-    a, b, c = l[0], l[1], int(l[2])
-    if a not in d1.keys():
-        d1[a] = d1.setdefault(a, {b: c})
-    else:
-        if b not in d1[a]:
-            d1[a].update({b: c})
-        else:
-            d1[a][b] = d1[a].get(b) + c
-
-print(d1)
-
-for k in sorted(d1):
-    print(k)
-    for k2, v2 in sorted(d1[k].items()):
-        print(k2, v2)
+for b in sorted(buyers):
+    print(b)
+    [print(f"{p}{' '}{sum(c)}") for p, c in sorted(buyers[b].items())]
