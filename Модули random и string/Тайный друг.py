563f82954eecd8997e8d386b3a5b8573433daba2
AlexVHub <alexvdata@gmail.com>
2023-05-15 23:26:41 +1100

    Done

--- Модули random и string/Тайный друг.py
+++ Модули random и string/Тайный друг.py
@@ -1,5 +1,20 @@
 import random
 
+result = []
 friends = [input() for _ in range(int(input()))]
-print(friends)
-#[print(friends.pop(0), '-', random.choice(friends)) for i in friends]
+count = ((len(friends) - 1) / 2) - 1
+for i in friends:
+    temp = []
+    for j in friends:
+        if j not in result and j != i:
+            temp.append(j)
+    while True:
+        choice = random.choice(temp)
+        if choice in result:
+            if choice == result.index(choice):
+                count -= 1
+                print(count)
+        result.append(choice)
+        break
+
+print(result)
