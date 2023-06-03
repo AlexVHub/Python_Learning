3c5ad8caf4444a4f72c576341b90df1fdd5549a4
AlexVHub <alexvdata@gmail.com>
2023-06-04 02:42:12 +1100

    Done

---
+++ Модули random и string/Monte Carlo method 2.py
@@ -0,0 +1,13 @@
+import random
+
+n = 10**6       # количество испытаний
+k = 0
+s = 4
+
+for _ in range(n):
+    x = random.uniform(-1, 1)
+    y = random.uniform(-1, 1)
+    if x ** 2 + y ** 2 <= 1:
+        k += 1
+
+print((k / n) * s)
